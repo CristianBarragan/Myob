c57fc7d34113c3476c8548f3f9ce944ae4f11420
cristianbarragan <cristian.barragan@gmail.com>
2017-06-25 09:43:19 +1200

    fix bottomRange

--- Storage/Data.cs
+++ Storage/Data.cs
@@ -8,7 +8,7 @@ namespace Storage
         public List<Tax> getTaxes()
         {
             List<Tax> taxes = new List<Tax>();
-            taxes.Add(new Tax { bottomRange = 0, upperRange = 18200, BaseTax = 0, variableTax = 0 });
+            taxes.Add(new Tax { bottomRange = 1, upperRange = 18200, BaseTax = 0, variableTax = 0 });
             taxes.Add(new Tax { bottomRange = 18201, upperRange = 37000, BaseTax = 0, variableTax = (decimal)0.19 });
             taxes.Add(new Tax { bottomRange = 37001, upperRange = 80000, BaseTax = 3572, variableTax = (decimal)0.325 });
             taxes.Add(new Tax { bottomRange = 80001, upperRange = 180000, BaseTax = 17547, variableTax = (decimal)0.37 });
